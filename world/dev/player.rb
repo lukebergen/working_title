96cf44605316e70770f919b7e81fc284d5ef0e1f
Luke Bergen <lukeabergen@gmail.com>
2016-12-23 14:29:16 -0500

    Fixing walk animations so they stop when user stops moving

--- world/dev/player.rb
+++ world/dev/player.rb
@@ -1,7 +1,8 @@
 on :loaded do
   # on x (left/right) 0 is stand still, -1 is move left, +1 is move right. On y (up/down), 0 is stand still, -1 is up, and +1 is down. So {x: 1, y: 0} means moving right. {x: -1, y: 1} represents moving left and down (diagonally).
   @movement = {x: 0, y: 0}  # start out standing still
-  set_animation("idle")
+  animation_name = "idle_" + @attributes["direction"]
+  set_animation(animation_name)
 end
 
 on :update do
@@ -11,26 +12,23 @@ on :update do
 end
 
 on :key_down do |key|
-  previous_direction = attributes["direction"]
-
   case key
   when :left
     @movement[:x] -= @attributes["speed"]
-    attributes["direction"] = "left"
+    @attributes["direction"] = "left"
+    set_animation("walk_left")
   when :right
     @movement[:x] += @attributes["speed"]
-    attributes["direction"] = "right"
+    @attributes["direction"] = "right"
+    set_animation("walk_right")
   when :up
     @movement[:y] -= @attributes["speed"]
-    attributes["direction"] = "up"
+    @attributes["direction"] = "up"
+    set_animation("walk_up")
   when :down
     @movement[:y] += @attributes["speed"]
     @attributes["direction"] = "down"
-  end
-
-  if @attributes["direction"] != previous_direction
-    animation_name = "walk_" + @attributes["direction"]
-    set_animation(animation_name)
+    set_animation("walk_down")
   end
 end
 
@@ -47,18 +45,18 @@ on :key_up do |key|
   end
 
   if @movement[:x] == 0 && @movement[:y] == 0
-    #set_animation("idle")
+    idle_animation = "idle_" + @attributes["direction"]
+    set_animation(idle_animation)
   end
 end
-
 __END__
 {
   "id": "player",
   "class": "player",
   "state": "idle",
-  "direction": "up",
-  "x": 200,
-  "y": 172,
+  "direction": "right",
+  "x": 224,
+  "y": 204,
   "z": 3,
   "speed": 4
 }
